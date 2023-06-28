494792cce4f7bfa6a026e29ac38ecffacc60c879
arpansaha13 <arpansaha1300@gmail.com>
2023-06-28 18:29:57 +0530

    chore: update deps and add html-validator

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
     '@nuxtjs/i18n',
 
     // https://v1.image.nuxtjs.org/
-    '@nuxt/image-edge',
+    '@nuxt/image',
 
     // https://nuxt.com/modules/icon
     'nuxt-icon',
@@ -25,11 +25,35 @@ export default defineNuxtConfig({
 
     // https://nuxt.com/modules/fontaine
     '@nuxtjs/fontaine',
+
+    // https://html-validator.nuxtjs.org/
+    '@nuxtjs/html-validator',
   ],
   css: ['@unocss/reset/tailwind.css', 'assets/main.css'],
   colorMode: {
     preference: 'dark',
     classPrefix: '',
     classSuffix: '',
+  },
+  htmlValidator: {
+    usePrettier: false,
+    logLevel: 'verbose',
+    failOnError: false,
+    options: {
+      extends: ['html-validate:document', 'html-validate:recommended', 'html-validate:standard'],
+      rules: {
+        'svg-focusable': 'off',
+        'no-unknown-elements': 'error',
+        // Conflicts or not needed as we use prettier formatting
+        'void-style': 'off',
+        'no-trailing-whitespace': 'off',
+        // Conflict with Nuxt defaults
+        'require-sri': 'off',
+        'attribute-boolean-style': 'off',
+        'doctype-style': 'off',
+        // Unreasonable rule
+        'no-inline-style': 'off',
+      },
+    },
   },
 })

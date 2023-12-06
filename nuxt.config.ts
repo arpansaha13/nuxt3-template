02f13756f43819b78278478ba5f4c60bff4b7308
arpansaha13 <arpansaha1300@gmail.com>
2023-12-06 19:35:11 +0530

    chore: remove title from nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,6 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'en',
       },
-      title: 'Nuxt Template',
     },
   },
   css: ['@unocss/reset/tailwind.css', 'assets/main.css'],

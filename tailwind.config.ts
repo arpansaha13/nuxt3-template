254e3d2c8d9fb41d28e83d153def162acf63713f
arpansaha13 <arpansaha1300@gmail.com>
2024-03-09 10:24:56 +0530

    style: add breakpoint for extra large screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,13 @@
 import type { Config } from 'tailwindcss'
 
 export default <Partial<Config>>{
+  theme: {
+    extend: {
+      screens: {
+        '3xl': '1920px', // extra large screens
+      },
+    },
+  },
   future: {
     hoverOnlyWhenSupported: true,
   },

a692e9a51b2a34ddd834fc25ef68bdcb80e07eff
arpansaha13 <arpansaha1300@gmail.com>
2024-12-28 12:01:19 +0530

    chore: re-enable default aspect-ratio utilities

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,18 @@
 import type { Config } from 'tailwindcss'
 
 export default <Partial<Config>>{
+  content: ['src/**/*.{ts,vue,css}'],
   theme: {
     extend: {
       screens: {
         '3xl': '1920px', // extra large screens
+      },
+      // Re-enable the default aspect-ratio utilities
+      // https://ui.nuxt.com/getting-started/installation#nuxtjstailwindcss
+      aspectRatio: {
+        auto: 'auto',
+        square: '1 / 1',
+        video: '16 / 9',
       },
     },
   },
